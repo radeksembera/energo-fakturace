71a0010f6bc269256dbda0fbbf0521f2574f9024
Radek Sembera <radek.sembera@gmail.com>
2025-11-25 14:32:46 +0100

    update

--- add_om_na_stranku_to_faktura.sql
+++
@@ -1,7 +0,0 @@
--- Migrace: Přidání sloupce 'om_na_stranku' do tabulky 'faktura'
--- Tento sloupec určuje, zda se v PDF přílohách 1 a 2 má každé odběrné místo tisknout na novou stránku
-
--- Přidání sloupce om_na_stranku (boolean, default FALSE)
-ALTER TABLE faktura ADD COLUMN om_na_stranku BOOLEAN DEFAULT FALSE;
-
--- Hotovo! Sloupec 'om_na_stranku' byl přidán do tabulky 'faktura'.
